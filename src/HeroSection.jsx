86dfc4b37885bfc23ae7d837600e03d3638df8f5
melaimmohamedanis <melaimmohamedanis@gmail.com>
2025-02-03 20:40:24 +0100

    description

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
               <span className="text-gray-700 font-bold">Professional</span> Web Developer
             </h2>
             <p className="text-gray-600 mt-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum consequat convallis.
+            'm a passionate Front-End / Full-Stack Web Developer with experience in building dynamic, user-friendly, and high-performance web applications. My expertise lies in React, Vite, Tailwind CSS, JavaScript, and modern web technologies. I focus on creating clean, maintainable code and delivering seamless user experiences.
             </p>
             <div className="mt-6 flex space-x-4">
               <button className="bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700 transition">
